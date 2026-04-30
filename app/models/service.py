c62045d5a3a443a456ab324523eb43894b1b64b2
yelasad <elasadbennordinehamissi@gmail.com>
2026-04-30 19:08:08 +0200

    feat(models): add users and service models

---
+++ app/models/service.py
@@ -0,0 +1,57 @@
+# app/models/service.py
+from decimal import Decimal
+from typing import Optional
+
+from sqlalchemy import String, Text, Integer, Numeric, ForeignKey, Enum as SQLEnum, text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.db.base import Base
+from app.models.mixins import TimestampMixin, SoftDeleteMixin
+from app.models.user import User
+from app.utils.enums import ServiceStatus
+
+#if TYPE_CHECKING:
+    #from app.models.category import Category
+
+
+class Service(Base, TimestampMixin, SoftDeleteMixin):
+    __tablename__ = "services"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    seller_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+        index=True,
+    )
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("categories.id", ondelete="RESTRICT"),
+        nullable=False,
+        index=True,
+    )
+
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+    image_url: Mapped[Optional[str]] = mapped_column(String(500), nullable=True)
+
+    status: Mapped[ServiceStatus] = mapped_column(
+        SQLEnum(ServiceStatus, name="service_status"),
+        nullable=False,
+        default=ServiceStatus.ACTIVE,
+        server_default=ServiceStatus.ACTIVE.value,
+        index=True,
+    )
+
+    average_rating: Mapped[Optional[float]] = mapped_column(
+        Numeric(3, 2), nullable=True
+    )
+    reviews_count: Mapped[int] = mapped_column(
+        Integer, nullable=False, default=0, server_default=text("0")
+    )
+
+    seller: Mapped["User"] = relationship(back_populates="services")
+    # category: Mapped["Category"] = relationship(back_populates="services")
+
+    def __repr__(self) -> str:
+        return f"<Service id={self.id} title={self.title} seller_id={self.seller_id}>"
---
+++ app/models/user.py
@@ -0,0 +1,42 @@
+from typing import Optional
+from sqlalchemy import String, Boolean,Enum as SQLEnum, text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.db.base import Base
+from app.utils.enums import UserRole
+from app.models.mixins import TimestampMixin, SoftDeleteMixin
+from app.models.service import Service
+
+class User(Base, TimestampMixin, SoftDeleteMixin):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+
+    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True, index=True)
+
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    avatar_url: Mapped[str] = mapped_column(String(500), nullable=True)
+    bio:  Mapped[Optional[str]] = mapped_column(String(1000), nullable=True)
+    university: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
+
+    role: Mapped[UserRole] = mapped_column(
+        SQLEnum(UserRole, name="user_role"),
+        nullable=False,
+        default=UserRole.USER,
+        server_default=UserRole.USER.value,
+    )
+
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True, server_default=text("true"))
+    is_suspended: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False, server_default=text("false"))
+
+    services: Mapped[list["Service"]] = relationship(
+        back_populates="seller",
+        foreign_keys="Service.seller_id",
+        passive_deletes=True,
+    )
+
+    def __repr__(self) -> str:
+        return f"<User id={self.id} email={self.email} role={self.role.value}>"
