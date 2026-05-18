c36a1f2a2d651b07919b90c0fd779049cd0e5c8d
yelasad <elasadbennordinehamissi@gmail.com>
2026-05-18 09:08:02 +0200

    fix(alembic): fixing import models in init file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
 from app.models.category import Category as Category
 from app.models.service import Service as Service
 from app.models.user import User as User
+from app.models.order import Order as Order
+from app.models.review import Review as Review
+from app.models.conversation import Conversation as Conversation
+from app.models.message import Message as Message
